9bda582c4f5aba1153e8738c56625ed6451da886
Alknight17 <knightalex17@gmail.com>
2022-07-31 19:34:31 -0400

    basic react frontend layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import NavBar from './components/NavBar';
+import { Routes, Route } from 'react-router-dom'
 // import Login from './components/Login';
 // import Signup from './components/Signup';
 import RecipeList from './components/RecipeList';
 import MyBar from './components/MyBar'
+import Layout from './components/Layout'
+
+
+// function App() {
+//   return (
+//     <div>
+//       <NavBar></NavBar>
+//       <main>
+//         <RecipeList></RecipeList>
+//         <MyBar></MyBar>
+        
+//       </main>
+
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div>
-      <NavBar></NavBar>
-      <main>
-        <RecipeList></RecipeList>
-        <MyBar></MyBar>
-        
-      </main>
-
-    </div>
+   <>
+    <Routes>
+    <Route path="/" element={<Layout />}/>
+    <Route path="/myBar" element={<MyBar />}/>
+    </Routes>
+   </>
   );
 }
 
--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,30 +1,65 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-function NavBar() {
+// function NavBar() {
 
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">
-            Home
-            </a>
-          </li>
-          <li>
-            <a href='/recipelist'>
-              My Bar
-            </a>
-          </li>
-          <li>
-            <a href='/login'>
-              Login/Logout
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+//   return (
+//     <header>
+//       <nav>
+//         <ul>
+//           <li>
+//             <a href="/">
+//             Home
+//             </a>
+//           </li>
+//           <li>
+//             <a href='/recipelist'>
+//               My Bar
+//             </a>
+//           </li>
+//           <li>
+//             <a href='/login'>
+//               Login/Logout
+//             </a>
+//           </li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
+
+const NavBar = () => (
+  <header>
+    <nav>
+
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="home-nav"
+        to="/home"
+      >
+        <FontAwesomeIcon icon={fahome} color="#4d4d4e" />
+      </NavLink>
+
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="mybar-nav"
+        to="/mybar"
+      >
+        <FontAwesomeIcon icon={fahome} color="#4d4d4e" />
+      </NavLink>
+
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="login-nav"
+        to="/login"
+      >
+        <FontAwesomeIcon icon={fahome} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+  </header>
+);
 
 export default NavBar;
