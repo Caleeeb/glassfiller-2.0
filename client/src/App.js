c6539c1041942acc89a2ad69ecf3300868f6d310
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-08-01 12:55:53 -0400

    fixed some errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
-import MyBar from './Pages/MyBar';
-import Home from './Pages/Home';
-import Login from './Pages/Login';
+import MyBar from './pages/MyBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer'
 
 
 function App() {
   return (
-    <>
-    <NavBar></NavBar>
+  <div>
+    <NavBar />
    
     <Routes>
-    <Route index element={<Home />} />
-    <Route path="MyBar" element={<MyBar />}/>
-    <Route path="Login" element={<Login />}/>
+      <Route index element={<Home />} />
+      <Route path="MyBar" element={<MyBar />}/>
+      <Route path="Login" element={<Login />}/>
     </Routes>
     
-    <Footer></Footer>
-   </>
+    <Footer />
+    </div>
   )
 }
 
