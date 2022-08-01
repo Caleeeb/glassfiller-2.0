1f7f26468bef9fdd75052f34ec989f60a9508b3f
Caleb Day <calebday@Calebs-MBP.lan>
2022-08-01 11:25:57 -0400

    queries started on the frontend

--- client/src/components/RecipeList/index.js
+++ client/src/components/RecipeList/index.js
@@ -1,41 +1,54 @@
 import React from 'react';
 
-const RecipeList = ({}) => {
+const ThoughtList = ({ thoughts, title }) => {
+    if (!thoughts.length) {
+        return <h3>No Thoughts Yet</h3>;
+    }
 
-    return (
-        <header>
-            <hi>
-                Filter
-                </hi>
-            <nav>
-                <section>
-                <ul>
-                    <li>
-                        First Ingredient
-                        <button
-                        id="add-ingredient"
-                        > + </button>
-                    </li>
-                    <li>
-                        Second Ingredient
-                        <button
-                        id="add-ingredient"
-                        > + </button>
-                    </li>
-                    <li>
-                        Third Ingredient
-                        <button
-                        id="add-ingredient"
-                        > + </button>
-                    </li>
-                    <li>
-                    <a href="add-ingredient" role="button">Add Ingredient</a>
-                    </li>
-                </ul>
-                </section>
-            </nav>
-        </header>
-    )
+    const RecipeList = ({ recipes, title }) => {
+        if (!recipes.length) {
+            return <h3>No Recipes Yet</h3>
+        }
+
+        return (
+            <div>
+                <h1>
+                    {title}
+                </h1>
+                {recipes &&
+                    recipes.map(recipe => (
+                        <div key={recipe._id} className="">
+                            <p className=""> {recipe.username} </p>
+                        </div>
+                    ))}
+                {/* <section>
+                        <ul>
+                            <li>
+                                First Ingredient
+                                <button
+                                    id="add-ingredient"
+                                > + </button>
+                            </li>
+                            <li>
+                                Second Ingredient
+                                <button
+                                    id="add-ingredient"
+                                > + </button>
+                            </li>
+                            <li>
+                                Third Ingredient
+                                <button
+                                    id="add-ingredient"
+                                > + </button>
+                            </li>
+                            <li>
+                                <a href="add-ingredient" role="button">Add Ingredient</a>
+                            </li>
+                        </ul>
+                    </section> */}
+            </div>
+        )
+    }
 }
 
 
