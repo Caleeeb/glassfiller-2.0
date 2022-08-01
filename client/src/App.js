75b4230e64262ee05c172831aedd0da09818ab2f
Alknight17 <knightalex17@gmail.com>
2022-08-01 15:13:00 -0400

    renamed Pages folder to pages for correct pathing in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import MyBar from './pages/MyBar';
-import Home from './pages/Home';
+import Home from './pages/Home.js';
 import Login from './pages/Login';
 import NavBar from './components/NavBar';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 
 
 function App() {
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,9 @@
-
 import React from 'react';
 
 const Home = () => {
     return (
         <main>
-            Home
+            <Home />
         </main>
     )
 }
