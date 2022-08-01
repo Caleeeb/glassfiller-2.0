0cfc3553600ada3dbf6cf7118db006bd10c0c76e
Alknight17 <knightalex17@gmail.com>
2022-07-31 22:08:34 -0400

    finalize React layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { Routes, Route,  } from 'react-router-dom';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
 import MyBar from './Pages/MyBar';
 import Home from './Pages/Home';
-// import Login from './Pages/Login';
-// // import Layout from './components/Layout';
+import Login from './Pages/Login';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer'
 
 
 function App() {
   return (
-   <>
+    <>
+    <NavBar></NavBar>
    
     <Routes>
-    {/* <Route path="/" element={<Layout />}/> */}
     <Route index element={<Home />} />
     <Route path="MyBar" element={<MyBar />}/>
-    {/* <Route path="Login" element={<Login />}/> */}
+    <Route path="Login" element={<Login />}/>
     </Routes>
-   
+    
+    <Footer></Footer>
    </>
   )
 }
+
 
 export default App;
