89a4b940ef8f6303d44c37eed918c8b9d68dea2a
Alknight17 <knightalex17@gmail.com>
2022-08-01 14:29:25 -0400

    pushing merged branches

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,3 +1,11 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
-import { QUERY_THOUGHTS, QUERY_ME_BASIC } from "../utils/queries";
+// import { useQuery } from "@apollo/client";
+// import { QUERY_THOUGHTS, QUERY_ME_BASIC } from "../utils/queries";
+
+const Home = () => {
+    return (
+        <h1>Home</h1>
+    )
+}
+
+export default Home;
