7d124458c0337628b08d52818c0969b59862a5ba
Caleb Day <calebday@Calebs-MBP.lan>
2022-08-02 23:41:47 -0400

    small bracket fix.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -35,6 +35,7 @@ const typeDefs = gql`
       login(email: String!, password: String!): Auth
       addUser(username: String!, email: String!, password: String!): Auth
       addRecipe(input: RecipeInput): Recipe
+    }
     
     input RecipeInput {
       title: String
