3a55a715acb389cb45dc069749c575491499807f
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-08-04 10:50:23 -0400

    updated styling

--- client/src/components/RecipeList/index.js
+++ client/src/components/RecipeList/index.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const RecipeList = ({ recipes, title }) => {
   const [show, setShow] = useState(true);
 
   return (
-    <main>
-      <div>
+    <main className="all-cards meow">
+      <div className="card-container meow">
         <h1>{title}</h1>
         {recipes &&
           recipes.map((recipe) => (
-            <div key={recipe._id} className="">
+            <div key={recipe._id} className="card">
               <p className=""> {recipe.title} </p>
               <p>{recipe.definition}</p>
 
               {show ? (
                 <div>
-                  <p className="">Recipe Submitted by: {recipe.username} </p>
+                 
+                  <p className=""><FontAwesomeIcon icon={faUser} color="#4d4d4e" /><br />Recipe Submitted by: {recipe.username} </p>
                   <ul>
                     {recipe.ingredients.map((ingredient, i) => (
                       <li> {ingredient}</li>
@@ -23,10 +26,13 @@ const RecipeList = ({ recipes, title }) => {
                   </ul>
                 </div>
               ) : null}
-
-              <button key={recipe._id} onClick={() => setShow(!show)}>
-                Toggle
+              <div className="button-container"> 
+              <div className="button-center"> 
+                <button className="action" key={recipe._id} onClick={() => setShow(!show)}>
+                Show Recipe
               </button>
+              </div>
+              </div>  
             </div>
           ))}
       </div>
